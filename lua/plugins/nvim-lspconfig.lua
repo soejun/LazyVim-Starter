77254d6d76af0286b2c115c4915bf07910f8d8ff
soejun <chan.wilson.wc@gmail.com>
2024-12-02 18:27:01 -0500

    Adds bash and html to nvim-lsp

---
+++ lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,20 @@
+local lspconfig = require("lspconfig")
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- Useful for debugging formatter issues
+      format_notify = true,
+      inlay_hints = { enabled = false },
+      servers = {
+        bashls = {
+          filetypes = { "sh", "zsh" },
+        },
+        html = {
+          filetypes = { "html", "templ", "htmldjango" },
+        },
+      },
+    },
+  },
+}
