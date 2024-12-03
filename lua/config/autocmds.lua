d4cc4db972a7282ac2a2a774e4e70ac67591316b
soejun <chan.wilson.wc@gmail.com>
2024-12-02 19:03:41 -0500

    Add gitlab_ci_ls lsp configuration

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  desc = "Ensure gitlab_ci_ls is properly activated for gitlab ci files",
+  pattern = { "*.gitlab-ci*.{yml,yaml}", "kaniko-build.yml" },
+  callback = function()
+    vim.bo.filetype = "yaml.gitlab"
+  end,
+})
