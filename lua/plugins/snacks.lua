9d56f2cab778d9486e0deccb139bef2c44ccdc46
soejun <chan.wilson.wc@gmail.com>
2024-12-17 10:56:37 -0500

    Increase snacks scroll speed animation

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,8 +3,13 @@ return {
   opts = {
     terminal = {
       win = {
-        height = 0.25
-      }
-    }
-  }
+        height = 0.25,
+      },
+      scroll = {
+        animate = {
+          duration = { step = 15, total = 150},
+        },
+      },
+    },
+  },
 }
