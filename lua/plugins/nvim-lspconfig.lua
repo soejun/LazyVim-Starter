267086a1a92d0d91331e86a2e7da8e6e4919e423
soejun <chan.wilson.wc@gmail.com>
2024-12-23 15:38:47 -0500

    (Mostly) stop pyright and ruff from showing the same duplicate errors

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,10 +4,37 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      ---@type lspconfig.options
       -- Useful for debugging formatter issues
       format_notify = true,
       inlay_hints = { enabled = false },
       servers = {
+        ---@type lspconfig.options.basedpyright
+        basedpyright = {
+          settings = {
+            basedpyright = {
+              analysis = {
+                typeCheckingMode = "standard",
+                diagnosticSeverityOverrides = {
+                  reportOptionalMemberAccess = "none",
+                  reportUnusedVariable = "none",
+                  reportUnusedCallResult = "none",
+                  reportUnusedExpression = "none",
+                  reportUnknownMemberType = "none",
+                  reportUnknownLambdaType = "none",
+                  reportUnknownParameterType = "none",
+                  reportUnknownVariableTypeType = "none",
+                  reportMissingParameterType = "none",
+                  reportMissingTypeStub = "information",
+                  reportUnknownVariableType = "none",
+                  reportUnknownArgumentType = "none",
+                  reportImplicitOverride = "none",
+                  reportAny = "none",
+                },
+              },
+            },
+          },
+        },
         bashls = {
           filetypes = { "sh", "zsh" },
         },
@@ -15,6 +42,30 @@ return {
           filetypes = { "html", "templ", "htmldjango" },
         },
         gitlab_ci_ls = {},
+        ---@type lspconfig.options.pyright
+        pyright = {
+          capabilities = (function()
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+            return capabilities
+          end)(),
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                -- diagnosticMode = "workspace",
+                diagnosticSeverityOverrides = {
+                  reportGeneralTypeIssues = "information",
+                  reportPrivateImportUsage = "information",
+                  reportOptionalOperand = "information",
+                  reportOptionalSubscript = "information",
+                  reportOptionalMemberAccess = "information",
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
