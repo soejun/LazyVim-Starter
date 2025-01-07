37dae9bd7ebf72be8cd2c023a038f766135586cf
soejun <chan.wilson.wc@gmail.com>
2025-01-07 16:33:36 -0500

    Enable gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,6 @@
 return {
-  "folke/tokyonight.nvim",
+  "LazyVim/LazyVim",
   opts = {
-    transparent = true,
-    styles = {
-      sidebars = "transparent",
-      floats = "transparent",
-    },
-    on_highlights = function(highlight, color)
-      highlight.WinSeparator = { fg = "#636da6" }
-    end,
+    colorscheme = "gruvbox",
   },
 }
