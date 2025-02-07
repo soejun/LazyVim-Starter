641888f474de9699b506ce913607badac3d275c5
soejun <chan.wilson.wc@gmail.com>
2025-02-07 13:12:12 -0500

    (Workaround): Change highlight groups for snacks explorer
    - add gruvbox pr for blink.cmp

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -21,7 +21,21 @@ return {
     inverse = true, -- invert background for search, diffs, statuslines and errors
     contrast = "hard", -- can be "hard", "soft" or empty string
     palette_overrides = {},
-    overrides = {},
+    overrides = {
+      -- snacks compatibility
+      -- NonText = { fg = "#928374"} -- gray, snacks compatibility generic
+      SnacksPickerDir = { link = "GruvboxGray" },
+      SnacksDashboardDir = { link = "GruvboxGray" },
+      SnacksPickerBufFlags = { link = "GruvboxGray" },
+      SnacksPickerPathHidden = { link = "GruvboxGray" },
+      SnacksPickerKeymapRhs = { link = "GruvboxGray" },
+      SnacksPickerUnselected = { link = "GruvboxGray" },
+      SnacksPickerTotals = { link = "GruvboxGray" },
+      SnacksPickerPathIgnored = { link = "GruvboxGray" },
+      -- https://github.com/ellisonleao/gruvbox.nvim/pull/372/commits/4be28bb8afc8986f838162109229c0774b3c2260
+      -- BlinkCmpGhostText = { link = "GruvboxBg4" },
+      -- BlinkCmpSource = { link = "GruvboxGray" },
+    },
     dim_inactive = false,
     transparent_mode = true, -- enable true for snacks.nvim terminal compatibility
   },
